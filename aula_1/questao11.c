7ad865f25d109102c23d04305f914ff3f0bb5f46
Arthur-rdrg <rodrigues.arruda.arthur@gmail.com>
2025-03-14 16:00:34 -0300

    questao 11

---
+++ aula_1/questao11.c
@@ -0,0 +1,55 @@
+//Sobre algumas estruturas de dados, responda:
+
+//a) Qual a diferença entre as estruturas pilha e fila?
+     //Pilha: Segue o princípio LIFO (Last In, First Out), ou seja,
+     //o último elemento inserido é o primeiro a ser removido.
+     //Exemplos: pilhas de pratos ou de livros.
+
+     //Fila: Segue o princípio FIFO (First In, First Out), ou seja,
+     //o primeiro elemento inserido é o primeiro a ser removido.
+     //Exemplos: fila de supermercado ou de atendimento.
+
+//b) Quais as vantagens da fila? Quais suas desvantagens?
+     //Vantagens:
+     //Ideal para sistemas que processam dados em ordem,
+     //como impressoras e tarefas em filas de execução.
+     //Fácil de implementar para controle de fluxo, como em buffers.
+
+     //Desvantagens:
+     //Inserir e remover elementos pode ser menos eficiente em
+     //filas baseadas em arrays, devido à necessidade de mover elementos.
+     //Em filas dinâmicas, o uso de ponteiros consome mais memória.
+
+//c) Quais as vantagens da pilha? Quais suas desvantagens?
+     //Vantagens:
+     //Simplicidade na manipulação de elementos através das operações
+     //push (inserir) e pop (remover).
+     //Útil para resolver problemas como a inversão de sequências e
+     //a avaliação de expressões matemáticas.
+
+     //Desvantagens:
+     //Só permite acesso ao elemento no topo, o que pode ser uma
+     //limitação para alguns casos.
+     //Crescimento limitado ao espaço de memória disponível,
+     //especialmente em pilhas baseadas em arrays.
+
+//d) Quais as suas aplicações?
+     //Fila: Sistemas de gerenciamento de processos, controle de
+     //fluxo de dados, impressão de documentos, etc.
+    
+     //Pilha: Avaliação de expressões matemáticas, inversão de
+     //sequências, controle de chamadas de funções, etc.
+
+//e) Qual a diferença deles para a lista?
+     //Lista: Estrutura de dados que permite a inserção e remoção
+     //de elementos em qualquer posição.
+     //Pode ser implementada de forma simples (lista encadeada)
+     //ou complexa (lista duplamente encadeada, lista circular).
+     //Permite acesso direto a qualquer elemento, facilitando
+     //operações como busca e ordenação.
+
+     //Pilha e fila: Restrições de acesso aos elementos, com
+     //operações específicas para inserção e remoção.
+     //Pilhas e filas são casos específicos de listas, com
+     //operações simplificadas para atender a propósitos
+     //específicos de processamento de dados.
