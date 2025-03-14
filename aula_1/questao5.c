1712a52055a89d6cc455ce249d737f6bc009e62a
Arthur-rdrg <rodrigues.arruda.arthur@gmail.com>
2025-03-14 09:26:14 -0300

    questao 5

---
+++ aula_1/questao5.c
@@ -0,0 +1,8 @@
+//O que ´e um vetor?
+//a) Explique o funcionameno de um vetor e como ele ´e tratado no C.
+// Um vetor em C é uma estrutura de dados usada para armazenar uma sequência de elementos do mesmo tipo.
+// Ele é tratado como um bloco contínuo de memória, onde os elementos são armazenados sequencialmente e podem ser acessados por um índice.
+
+//b) Explique o que uma string e como ela é tratada em C.
+//   Uma string em C é um vetor de caracteres terminado por um caractere nulo ('\0').
+//   Ela é tratada como um vetor de caracteres, onde cada caractere é armazenado em uma posição do vetor.
